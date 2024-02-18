78a86760db73fc0c5ac7dbc92441e5e71a092802
Nicholas Ingles <nringles@student.rtc.edu>
2024-02-18 11:31:51 -0800

    Add project files.

---
+++ Conditions_Assignment_NicholasIngles/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pick an option:");
+Console.WriteLine("1. Determine the Greater Number");
+Console.WriteLine("2. Grade Calculator");
+Console.WriteLine("3. Leap Year Checker");
+Console.WriteLine("4. Number Classification");
+Console.WriteLine("5. Rock, Paper, Scissors Game\n");
+string input = Console.ReadLine();
+
+if (input == "1")
+{
+
+    Console.WriteLine("boo");
+
+}
+else if (input == "2")
+{
+    Console.Write("\nGrade Calculator selected.\nEnter grade score: ");
+    Console.ReadLine();
+
+}
+else if (input == "3")
+{
+    Console.WriteLine("kapow");
+
+}
+else if (input == "4")
+{
+    Console.WriteLine("powerjf");
+}
+else if (input == "5")
+{
+    Console.WriteLine("klall");
+
+}
+else
+{
+    Console.WriteLine("Invalid input. Try again.");
+}
